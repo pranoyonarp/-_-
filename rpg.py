3dc9a08434357de1f22469a42373df0b0ae79ea5
Pranoy Patra <143189321+pranoyonarp@users.noreply.github.com>
2023-10-26 18:11:17 +0530

    rpg.py

--- rpg.py
+++ rpg.py
@@ -1,42 +1,23 @@
 # COMMAND LINE RANDOM PASSWORD GENERATOR
+#random password generator
 import random
 print("enter the length of password you want to generate")
 length=int(input())
-print("enter the character set you prefer for your password")
+print("enter the character set you prefer for your password:1 for letters, 2 for numbers, 3 for symbols")
 choice=int(input())
 password=""
+letters="abcdefghijklmnopqrstuvwxyz"
+numbers="123456789"
+symbols="!@#$%^&*()_-=+[]{}:;,.<>/?~`"
 if choice==1:
-    listof_letters="abcdefghijklmnopqrstuvwxyz"
     for i in range(length):     
-        password=password+random.choice(listof_letters)
+        password=password+random.choice(letters)
     print(password)
 elif choice==2:
-    listof_letters="123456789"
     for i in range(length):     
-        password=password+random.choice(listof_letters)
+        password=password+random.choice(numbers)
     print(password)
 elif choice==3:
-    listof_letters="!@#$%^&*()_-=+[]{}:;,.<>/?~`"
     for i in range(length):     
-        password=password+random.choice(listof_letters)
-    print(password)
-elif choice==4:
-    listof_letters="abcdefghijklmnopqrstuvwxyz123456789"
-    for i in range(length):     
-        password=password+random.choice(listof_letters)
-    print(password)
-elif choice==5:
-    listof_letters="abcdefghijklmnopqrstuvwxyz!@#$%^&*()_-=+[]{}:;,.<>/?~`"
-    for i in range(length):     
-        password=password+random.choice(listof_letters)
-    print(password)
-elif choice==6:
-    listof_letters="123456789!@#$%^&*()_-=+[]{}:;,.<>/?~`"
-    for i in range(length):     
-        password=password+random.choice(listof_letters)
-    print(password)
-elif choice==7:
-    listof_letters="abcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_-=+[]{}:;,.<>/?~`"
-    for i in range(length):     
-        password=password+random.choice(listof_letters)
+        password=password+random.choice(symbols)
     print(password)
